3b2bbf4f7c1c2ad65aa59cf1143ebae3dee94c15
searchme86 <searchme86@gmail.com>
2022-01-13 19:17:45 +0900

    🔨 express server code inserted

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-// @ts-check
+//@ts-check
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(bodyParser.json());
+app.use('/public', express.static('src/public'));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.get('/customer', (req, res) => {
+  res.render(path.join(__dirname + '/views/customer/customer.html'));
+});
+
+app.get('/product', (req, res) => {
+  res.render(path.join(__dirname + '/views/products/product.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`the express server is listening at port ${PORT}`);
+});
